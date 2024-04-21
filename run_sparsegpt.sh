5e29d0ab60934f20c9427b38b503df72bb256f14
pprp <dongpeijie98@gmail.com>
2024-04-21 08:13:40 +0800

    Refactor pruning methods and update device configuration in prune.py, run.py, and run_sparsegpt.sh

--- run_sparsegpt.sh
+++ run_sparsegpt.sh
@@ -2,70 +2,70 @@
 
 # sparsegpt
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 128 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs128.log 2>&1 
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 128 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs128.log 2>&1 
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 64 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs64.log 2>&1 
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 64 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs64.log 2>&1 
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 32 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs32.log 2>&1
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 32 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs32.log 2>&1
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 16 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs16.log 2>&1
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 16 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method sparsegpt > ./logs/sparsegpt_f_4_l_4_bs16.log 2>&1
 
-# wanda 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 128 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method wanda > ./logs/wanda_f_4_l_4_bs128.log 2>&1
+# # wanda 
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 128 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method wanda > ./logs/wanda_f_4_l_4_bs128.log 2>&1
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 64 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method wanda > ./logs/wanda_f_4_l_4_bs64.log 2>&1
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 64 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method wanda > ./logs/wanda_f_4_l_4_bs64.log 2>&1
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 32 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method wanda > ./logs/wanda_f_4_l_4_bs32.log 2>&1
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 32 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method wanda > ./logs/wanda_f_4_l_4_bs32.log 2>&1
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
-    wikitext2 braq \
-    --blocksize 16 \
-    --salient_metric hessian \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --prune_method wanda > ./logs/wanda_f_4_l_4_bs16.log 2>&1
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
+#     wikitext2 braq \
+#     --blocksize 16 \
+#     --salient_metric hessian \
+#     --sparsity_ratio 0.5 \
+#     --sparsity_type 4:8 \
+#     --prune_method wanda > ./logs/wanda_f_4_l_4_bs16.log 2>&1
 
 # Quant Range (origin is -1, 1000)
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
@@ -76,8 +76,7 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyll
     --sparsity_type 4:8 \
     --prune_method sparsegpt \
     --minlayer 1 \
-    --maxlayer 19 \
-    --quant_range 0 1000 > ./logs/sparsegpt_f_4_l_4_bs128_mn1_mx19.log 2>&1
+    --maxlayer 19 > ./logs/sparsegpt_f_4_l_4_bs128_mn1_mx19.log 2>&1
 
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
     wikitext2 braq \
@@ -85,10 +84,9 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyll
     --salient_metric hessian \
     --sparsity_ratio 0.5 \
     --sparsity_type 4:8 \
-    --prune_method wanda \
+    --prune_method sparsegpt \
     --minlayer 2 \
-    --maxlayer 18 \
-    --quant_range 0 1000 > ./logs/wanda_f_4_l_4_bs128_mn2_mx18.log 2>&1
+    --maxlayer 18 > ./logs/sparsegpt_f_4_l_4_bs128_mn2_mx18.log 2>&1
 
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
     wikitext2 braq \
@@ -98,8 +96,7 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyll
     --sparsity_type 4:8 \
     --prune_method sparsegpt \
     --minlayer 3 \
-    --maxlayer 17 \
-    --quant_range 0 1000 > ./logs/sparsegpt_f_4_l_4_bs128_mn3_mx17.log 2>&1
+    --maxlayer 17 > ./logs/sparsegpt_f_4_l_4_bs128_mn3_mx17.log 2>&1
 
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
     wikitext2 braq \
@@ -107,10 +104,9 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyll
     --salient_metric hessian \
     --sparsity_ratio 0.5 \
     --sparsity_type 4:8 \
-    --prune_method wanda \
+    --prune_method sparsegpt \
     --minlayer 4 \
-    --maxlayer 16 \
-    --quant_range 0 1000 > ./logs/wanda_f_4_l_4_bs128_mn4_mx16.log 2>&1
+    --maxlayer 16 > ./logs/sparsegpt_f_4_l_4_bs128_mn4_mx16.log 2>&1
 
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyllama/tinyllama-1.1b-480k-1t \
     wikitext2 braq \
@@ -120,5 +116,4 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=7 python3 run.py /data2/share/tinyll
     --sparsity_type 4:8 \
     --prune_method sparsegpt \
     --minlayer 5 \
-    --maxlayer 15 \
-    --quant_range 0 1000 > ./logs/sparsegpt_f_4_l_4_bs128_mn5_mx15.log 2>&1
+    --maxlayer 15 > ./logs/sparsegpt_f_4_l_4_bs128_mn5_mx15.log 2>&1
