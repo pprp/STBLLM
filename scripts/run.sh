3f910a5d2ba3ef6a8f9cc31089b53c2f7183a933
pprp <dongpeijie98@gmail.com>
2024-05-16 10:53:29 +0800

    Support 3bit to Binarization Class

--- scripts/run.sh
+++ scripts/run.sh
@@ -260,10 +260,13 @@
 
 
 # 2bit 49.682358 for tinyllama
-CUDA_VISIBLE_DEVICES=6 python3 run.py /data/lujunli/hf_download/llama-2-7b c4 braq --blocksize 128 \
+CUDA_VISIBLE_DEVICES=3 python3 run.py /data/lujunli/hf_download/llama-2-7b c4 braq --blocksize 128 \
     --salient_metric hessian \
     --sparsity_ratio 0.5 \
-    --sparsity_type 4:8
+    --sparsity_type 4:8 > ./logs/billm_4mask_salient_braq.log 2>&1 &
+
+tail -f ./logs/billm_4mask_salient_braq.log
+
     # --prune_method ria_structure \
     # --reconstruction
     
--- utils/binary.py
+++ utils/binary.py
@@ -77,8 +77,59 @@ class Binarization(nn.Module):
             w = F.relu(w)
             w_int = (w / self.scale[groupi]).round().clamp(0, 1)
             w = w_int * self.scale[groupi]
-        elif self.method in ["2bit", "4bit"]:
+        # elif self.method in ["2bit", "4bit"]:
 
+        #     bits = int(self.method[0])
+        #     perchannel = True
+        #     weight = True
+        #     dev = w.device
+        #     maxq = torch.tensor(2**bits - 1)
+        #     scale = torch.zeros(1)
+        #     zero = torch.zeros(1)
+
+        #     if dev != scale.device:
+        #         scale = scale.to(dev)
+        #         zero = zero.to(dev)
+        #         maxq = maxq.to(dev)
+
+        #     x = w.clone()
+        #     shape = x.shape
+
+        #     if perchannel:
+        #         if weight:
+        #             x = x.flatten(1)
+        #         else:
+        #             if len(shape) == 4:
+        #                 x = x.permute([1, 0, 2, 3])
+        #                 x = x.flatten(1)
+        #             if len(shape) == 3:
+        #                 x = x.reshape((-1, shape[-1])).t()
+        #             if len(shape) == 2:
+        #                 x = x.t()
+        #     else:
+        #         x = x.flatten().unsqueeze(0)
+        #     tmp = torch.zeros(x.shape[0], device=dev)
+        #     xmin = torch.minimum(x.min(1)[0], tmp)
+        #     xmax = torch.maximum(x.max(1)[0], tmp)
+
+        #     tmp = (xmin == 0) & (xmax == 0)
+        #     xmin[tmp] = -1
+        #     xmax[tmp] = +1
+        #     scale = (xmax - xmin) / maxq
+        #     zero = torch.round(-xmin / scale)
+        #     if not perchannel:
+        #         if weight:
+        #             tmp = shape[0]
+        #         else:
+        #             tmp = shape[1] if len(shape) != 3 else shape[2]
+        #         scale = scale.repeat(tmp)
+        #         zero = zero.repeat(tmp)
+        #     if weight:
+        #         shape = [-1] + [1] * (len(shape) - 1)
+        #         scale = scale.reshape(shape)
+        #         zero = zero.reshape(shape)
+        #     w = normal_quantize(w, scale, zero, maxq)
+        elif self.method in ["2bit", "3bit", "4bit"]:
             bits = int(self.method[0])
             perchannel = True
             weight = True
@@ -129,6 +180,7 @@ class Binarization(nn.Module):
                 scale = scale.reshape(shape)
                 zero = zero.reshape(shape)
             w = normal_quantize(w, scale, zero, maxq)
+
         elif self.method == "prune":
             return torch.zeros_like(w)
         elif self.method == "ternary":
